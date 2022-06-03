1a0fff5d5d319a63368cf052203a79adba580ab5
tallesChristian <72113346+tallesChristian@users.noreply.github.com>
2022-06-03 18:23:07 -0300

    Adicionar arquivos de projeto.

---
+++ CaixaVilaMarilena/CaixaVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaVilaMarilena
+{
+    public class CaixaVM
+    {
+        public float _quantia { get; set; }
+
+
+        public float Depositar(float valor)
+        {
+            return _quantia += valor;
+        }
+    }
+}
